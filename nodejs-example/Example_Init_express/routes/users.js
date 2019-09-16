a6907c30f2af99e6831a46682c3ee0d94e6c29b7
Danila Balyakin <zx19-95@mail.ru>
2019-09-16 16:40:09 +0300

    stub users

--- nodejs-example/Example_Init_express/routes/users.js
+++ nodejs-example/Example_Init_express/routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [
+  {
+    name: "test",
+    password: "test",
+    type: "test",
+    email: "test@test.ru",
+  },
+  {
+    name: "test1",
+    password: "test1",
+    type: "test1",
+    email: "test1@test.ru",
+  },
+  {
+    name: "test2",
+    password: "test2",
+    type: "test2",
+    email: "test2@test.ru",
+  },
+  {
+    name: "test3",
+    password: "test3",
+    type: "test3",
+    email: "test3@test.ru",
+  },
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(users);
 });
 
 module.exports = router;
